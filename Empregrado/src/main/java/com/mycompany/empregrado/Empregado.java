143fd0a2d52fae6e35906feda5c3a91ea2177b47
Usuario <Usuario@DESKTOP-DJUMKHS>
2023-06-05 17:58:10 +0200

    engadidos setters e getters

---
+++ Empregrado/src/main/java/com/mycompany/empregrado/Empregado.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.empregrado;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Empregado {
+    private String nome;
+    private int Salario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getSalario() {
+        return Salario;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSalario(int Salario) {
+        this.Salario = Salario;
+    }
+    
+}
